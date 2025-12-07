ea6eb2df6626f93a6246a44a1da7d750bfe9a5f3
mahditheprogrammer <panahpourmohamadmahdi62@gmail.com>
2025-12-07 18:13:42 +0200

    working on matmul, next step is tiling and shared memory

--- LIB/mat_op.cu
+++ LIB/mat_op.cu
@@ -32,16 +32,17 @@ __global__ void matadd(float *t_A, float *t_b,float *output, int num_threads){
 Tensor Tensor::matmul(const Tensor& t_A, const Tensor& t_B){
         std::vector<int> shape_A = t_A.get_shape();
         std::vector<int> shape_B = t_B.get_shape();
-        int d=1;
-	std::vector<int> shape_output;
-        std::vector<int> shape = t_A.get_shape();
-        for(int f1=0; f1<shape.size()-2;f1++){
+  
+		std::vector<int> shape_output;
+		int d=1;
+
+        for(int f1=0; f1<shape_A.size()-2;f1++){
                 d*=shape_A[f1];
 		shape_output.push_back(shape_A[f1]);
         }
 
-	shape_output.push_back(shape_A[shape_A.size()-2]);
-	shape_output.push_back(shape_B[shape_B.size()-1]);
+		shape_output.push_back(shape_A[shape_A.size()-2]);
+		shape_output.push_back(shape_B[shape_B.size()-1]);
 
         float* add_A = t_A.device_address();
         float* add_B = t_B.device_address();
@@ -49,15 +50,18 @@ Tensor Tensor::matmul(const Tensor& t_A, const Tensor& t_B){
         float *add_C;
         float *h_C = new float[total_size_C];
         cudaMalloc((void**)&add_C,total_size_C * sizeof(float));
+
         dim3 blockDim(32,32);
         dim3 gridDim((shape_B[shape_B.size()-1]+31)/32,(shape_A[shape_A.size()-2]+31)/32 , d);
+
         matrixmultiplication<<<gridDim, blockDim>>>(add_A,add_B,add_C,d,shape_A[shape_A.size()-2],shape_B[shape_B.size()-2],shape_B[shape_B.size()-1]);
+
         cudaDeviceSynchronize();
         cudaMemcpy(h_C, add_C, total_size_C * sizeof(float), cudaMemcpyDeviceToHost);
-	cudaFree(add_C);
+		cudaFree(add_C);
 
-	Tensor output(shape_output);
-	output.from_list(h_C);
+		Tensor output(shape_output);
+		output.from_list(h_C);
 
         return output;
 }
